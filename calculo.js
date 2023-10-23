764fd22ecb254087a68855c46bd2e886a3538f83
McJoaoM <130366173+McJoaoM@users.noreply.github.com>
2023-10-23 08:46:20 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,23 @@
+
+function Calcular(){
+
+    var idadeCanina = document.querySelector(".idadeCanina");
+    var idadeCanina = prompt("Qual a idade do cachorrinho?");
+    
+    if (media >= 10){
+        alert ("seu cahorro é velho");
+    }   else {
+        alert ("Seu cachorro é nutela")
+    }
+
+    var idadeVelho = 18;
+    var idadeNovo = 10;
+    
+    var max = Math.max(idadeVelho, idadeNovo);
+    var min = Math.min(idadeVelho, idadeNovo);
+
+    mostraResultado.textContent = "Resultado: a idade humana do cachorrinho é " + idadeCanina;
+
+    //BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    //POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING
+}
